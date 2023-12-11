1f237ed82336d6e4180879944bfd4d09136271dd
William Whitelocke <wewhitelocke@gmail.com>
2023-12-10 20:41:58 -0500

    new changes from William

---
+++ listuser.php
@@ -0,0 +1,10 @@
+<?php
+
+    session_start();
+
+    $host = 'localhost';
+    $username = 'lab5_user';
+    $password = 'password123';
+    $dbname = 'dolphin_crm';
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    
