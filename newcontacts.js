647861e9b3aabc1825378cba5554990bf7305a52
William Whitelocke <wewhitelocke@gmail.com>
2023-12-10 22:59:43 -0500

    new changes from William

---
+++ newcontacts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    let save = document.getElementById("save");
+
+
+    save.addEventListener('click',() =>{
+        var fname = document.getElementById("firstname").value;
+        var lname = document.getElementById("lastname").value;
+        var email = document.getElementById("email")/value;
+        var pword = document.getElementById("password").value;
+        
+
+    });
+
+
+});
